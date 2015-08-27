83f03824097c7205fc5986162178b17645e3fb24
Jeff Wood <Jeff@Jeffs-MacBook-Pro.local>
2015-08-26 20:21:20 -0500

    Updated 'drawpixels' example

--- drawpixels/drawpixels.c
+++ drawpixels/drawpixels.c
@@ -1,42 +1,60 @@
 #include <GLUT/glut.h>      // (or others, depending on the system in use)
+#include <stdlib.h>
+
+int iWidth = 512;
+int iHeight = 512;
+int iSize;
+GLubyte *cPixels;
 
 void init (void){
 	glClearColor (1.0, 1.0, 1.0, 0.0);  // Set display-window color to white.
 
 	glMatrixMode (GL_PROJECTION);       // Set projection parameters.
 	gluOrtho2D (0.0, 200.0, 0.0, 150.0);
+
+	unsigned int iInc;
+	GLubyte cTemp;
+	
+
+	for(iInc = 0; iInc < iSize; iInc++){
+
+		cTemp = (GLubyte)((iHeight - (iInc % iHeight)) >> 1);
+		cPixels[iInc*3+0] = cTemp;
+		cTemp = (GLubyte)((iInc % iHeight) >> 1);
+		cPixels[iInc*3+2] = cTemp;
+
+	}
+
 }
 
-void bitPattern (void){
+void drawPixels (void){
 	glClear (GL_COLOR_BUFFER_BIT);  // Clear display window.
 
 
 	glColor3i(0,0,0);
-	GLubyte bitShape [20] = {
-		0x1c, 0x00, 0x1c, 0x00, 0x1c, 0x00, 0x1c, 0x00, 0x1c, 0x00,
-		0xff, 0x80, 0x7f, 0x00, 0x3e, 0x00, 0x1c, 0x00, 0x08, 0x00};
 
-	glPixelStorei (GL_UNPACK_ALIGNMENT, 1);  // Set pixel storage mode.
+	glDrawPixels(iWidth, iHeight, GL_RGB, GL_UNSIGNED_BYTE, cPixels);
 
-	glRasterPos2i (10, 10);
-	glBitmap (9, 10, 0.0, 0.0, 20.0, 15.0, bitShape);
-
-
-	glEnd ( );
 
 	glFlush ( );     // Process all OpenGL routines as quickly as possible.
 }
 
 int main (int argc, char* argv[]){
+
+	iSize = iWidth * iHeight;
+	cPixels = malloc(3*sizeof(GLubyte)*iSize);
+
 	glutInit (&argc, argv);                         // Initialize GLUT.
 	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);   // Set display mode.
 	glutInitWindowPosition (50, 100);   // Set top-left display-window position.
-	glutInitWindowSize (400, 300);      // Set display-window width and height.
+	glutInitWindowSize (iWidth, iHeight);      // Set display-window width and height.
 	glutCreateWindow ("An Example OpenGL Program"); // Create display window.
 
 	init ( );                            // Execute initialization procedure.
-	glutDisplayFunc (bitPattern);       // Send graphics to display window.
+	glutDisplayFunc (drawPixels);       // Send graphics to display window.
 	glutMainLoop ( );                    // Display everything and wait.
+
+	free(cPixels);
 
 	return 0;
 }
